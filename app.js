98e69cb73d611f28057fea489282261a24cefb38
niksankarkee <karkeeniksan@gmail.com>
2020-08-24 22:45:47 +0900

    'ecommerceSetup'

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('Hello world');
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const path = require('path');
+
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminRoutes);
+
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
