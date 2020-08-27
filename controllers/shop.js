4c28f27d81bbd13909b232382fc28081325be87d
niksankarkee <karkeeniksan@gmail.com>
2020-08-27 21:42:05 +0900

    'addCart'

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 
 exports.getIndex = (req, res, next) => {
@@ -20,6 +21,18 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
+exports.getProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        res.render('shop/product-detail', {
+            product: product,
+            pageTitle: product.title,
+            path: 'products'
+        })
+    });
+
+};
+
 
 exports.getCarts = (req, res, next) => {
     res.render('shop/cart', {
@@ -27,6 +40,16 @@ exports.getCarts = (req, res, next) => {
         pageTitle: 'Your Cart'
     });
 };
+
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.findById(prodId, (product) => {
+        Cart.addProduct(prodId, product.price);
+    });
+    console.log(prodId);
+    res.redirect('/cart');
+};
+
 exports.getOrders = (req, res, next) => {
     res.render('shop/orders', {
         path: '/orders',
