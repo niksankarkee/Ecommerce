6051f3d095c93adfcf90609ca6509ad72e21d5b2
niksankarkee <karkeeniksan@gmail.com>
2020-08-29 13:00:10 +0900

    'mySqlUsingQuery'

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const path = require('path');
 
 const get404Controller = require('./controllers/error');
 
+const db = require('./util/database');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -15,6 +17,7 @@ app.set('views', 'views');
 const adminRouts = require('./routes/admin');
 
 const shopRoutes = require('./routes/shop');
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
